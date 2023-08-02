c0ede3f06ee6152e8f80b875b8eb399311cd77b7
Tungki Reza Prasakti <tungki@yandex.com>
2023-08-02 16:29:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    let myArray = []
+    for(let i = 0; i < 100; i++){
+        myArray.push(i)
+    }
+    res.send(`Yo, ${myArray}`)
 })
 app.listen(process.env.PORT || 3000)
